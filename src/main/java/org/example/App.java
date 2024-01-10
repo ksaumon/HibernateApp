4f56f8ebbb721490e77665e8e437f8d5ea52e4a8
Ksaumon <97893722+ksaumon@users.noreply.github.com>
2024-01-10 13:57:45 +0300

    обновление, удаление, добавление сущьности в bd

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -25,6 +25,51 @@ public class App {
 //        }
 //    }
 
+
+    //добавление в bd
+//    public static void main( String[] args ) {
+//        Configuration configuration = new Configuration().addAnnotatedClass(Person.class);
+//
+//        SessionFactory sessionFactory = configuration.buildSessionFactory();
+//        Session session = sessionFactory.getCurrentSession();
+//        try {
+//            session.beginTransaction();
+//            Person person1 = new Person("test1", 30);
+//           public static void main( String[] args ) {
+//        Configuration configuration = new Configuration().addAnnotatedClass(Person.class);
+//
+//        SessionFactory sessionFactory = configuration.buildSessionFactory();
+//        Session session = sessionFactory.getCurrentSession();
+//        try {
+//            session.beginTransaction();
+//            Person person1 = new Person("test1", 30);
+//            Person person2 = new Person("test2", 40);
+//            Person person3 = new Person("test3", 50);
+//
+//            session.save(person1);
+//            session.save(person2);
+//            session.save(person3);
+//
+//            session.getTransaction().commit();
+//
+//        } finally {
+//            sessionFactory.close();
+//        }
+//    }
+//            Person person3 = new Person("test3", 50);
+//
+//            session.save(person1);
+//            session.save(person2);
+//            session.save(person3);
+//
+//            session.getTransaction().commit();
+//
+//        } finally {
+//            sessionFactory.close();
+//        }
+//    }
+
+    //обновление, удаление, добавление сущьности в bd
     public static void main( String[] args ) {
         Configuration configuration = new Configuration().addAnnotatedClass(Person.class);
 
@@ -32,15 +77,14 @@ public class App {
         Session session = sessionFactory.getCurrentSession();
         try {
             session.beginTransaction();
-            Person person1 = new Person("test1", 30);
-            Person person2 = new Person("test2", 40);
-            Person person3 = new Person("test3", 50);
-
-            session.save(person1);
-            session.save(person2);
-            session.save(person3);
-
+            //Person person = session.get(Person.class, 2);
+            //person.setName("New name");// обновление имени
+           // session.delete(person);//удаление сушьности
+            Person person = new Person("Some name", 60);
+            session.save(person);// сохранение сушьности
             session.getTransaction().commit();
+
+            System.out.println(person.getId());
 
         } finally {
             sessionFactory.close();
