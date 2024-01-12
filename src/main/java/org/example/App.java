9fadbc7a2cc8b220c03b7042198e57e0d2491134
Ksaumon <97893722+ksaumon@users.noreply.github.com>
2024-01-12 13:00:23 +0300

    пример использования языка HQL

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -6,6 +6,8 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.util.List;
+
 public class App {
 //    public static void main( String[] args ) {
 //        Configuration configuration = new Configuration().addAnnotatedClass(Person.class);
@@ -70,6 +72,29 @@ public class App {
 //    }
 
     //обновление, удаление, добавление сущьности в bd
+//    public static void main( String[] args ) {
+//        Configuration configuration = new Configuration().addAnnotatedClass(Person.class);
+//
+//        SessionFactory sessionFactory = configuration.buildSessionFactory();
+//        Session session = sessionFactory.getCurrentSession();
+//        try {
+//            session.beginTransaction();
+//            //Person person = session.get(Person.class, 2);
+//            //person.setName("New name");// обновление имени
+//           // session.delete(person);//удаление сушьности
+//            Person person = new Person("Some name", 60);
+//            session.save(person);// сохранение сушьности
+//
+//            session.getTransaction().commit();
+//
+//            System.out.println(person.getId());
+//
+//        } finally {
+//            sessionFactory.close();
+//        }
+//    }
+
+// пример использования языка HQL
     public static void main( String[] args ) {
         Configuration configuration = new Configuration().addAnnotatedClass(Person.class);
 
@@ -77,15 +102,23 @@ public class App {
         Session session = sessionFactory.getCurrentSession();
         try {
             session.beginTransaction();
-            //Person person = session.get(Person.class, 2);
-            //person.setName("New name");// обновление имени
-           // session.delete(person);//удаление сушьности
-            Person person = new Person("Some name", 60);
-            session.save(person);// сохранение сушьности
+            //List<Person> people = session.createQuery("FROM Person").getResultList();//вывод всех столбцов
+            //List<Person> people = session.createQuery("FROM Person where age >30").getResultList();
+            // вывод всех с возрастом больше 30
+            //List<Person> people = session.createQuery("FROM Person where name LIKE 't%'").getResultList();
+            // вывод всех у кого имя начинаеться на t (% - любое количество символов после t)
+            //int updatedRows = session.createQuery("UPDATE Person set name='Jon' where age > 30")
+            //        .executeUpdate();
+            //обновляет всех людей у которых возраст больше 30
+            int updatedRows = session.createQuery("delete from Person where age > 30 and age < 60")
+                    .executeUpdate();
+            //удаляет всех людей с возрастом больше 30 и меньше 60
+            System.out.println("Количество обновленных записей: " + updatedRows);
+//
+//            for (Person person : people)
+//                System.out.println(person.getName() + "," + person.getAge());
+
             session.getTransaction().commit();
-
-            System.out.println(person.getId());
-
         } finally {
             sessionFactory.close();
         }
